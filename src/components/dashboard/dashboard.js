86d63d23872f0a4086a5a9790e794ab565625583
Katy Robinson <katyorob@gmail.com>
2018-10-29 17:41:54 -0700

    removed mock request from dashboard

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -29,89 +29,63 @@ class Dashboard extends React.Component {
     }
   }
 
-  mockRequest = () => {
-    console.log(JSON.parse(request));
-    return JSON.parse(request);
+  forecastSearch = (city, zipCode) => {
+    if (city) {
+      return superagent.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city},us&units=imperial&APPID=${APIKEY}`)
+        .then((response) => {
+          console.log(response);
+          this.setState(() => {
+            return {
+              forecast: response.body.list.map((results) => {
+                return {
+                  temp: results.main.temp,
+                  temp_min: results.main.temp_min,
+                  temp_max: results.main.temp_max,
+                  humidity: results.main.humidity,
+                  weather_main: results.weather[0].main,
+                  weather_description: results.weather[0].description,
+                  date: results.dt_txt,
+                };
+              }).filter(forecast => forecast.date.includes('12:00:00')),
+              city: response.body.city.name,
+            };
+          })
+          try {
+            localStorage.forecast = JSON.stringify(this.state.forecast);
+          } catch (err) {
+            console.log(err);
+            this.setState({ forecast: null, successfulSearch: false });
+          }
+        });
+    }
+    else if (zipCode) {
+      return superagent.get(`http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode},us&units=imperial&APPID=${APIKEY}`)
+        .then((response) => {
+          console.log(response);
+          this.setState(() => {
+            return {
+              forecast: response.body.list.map((results) => {
+                return {
+                  temp: results.main.temp,
+                  temp_min: results.main.temp_min,
+                  temp_max: results.main.temp_max,
+                  humidity: results.main.humidity,
+                  weather_main: results.weather[0].main,
+                  weather_description: results.weather[0].description,
+                  date: results.dt_txt,
+                };
+              }).filter(forecast => forecast.date.includes('12:00:00')),
+              city: response.body.city.name,
+            };
+          })
+          try {
+            localStorage.forecast = JSON.stringify(this.state.forecast);
+          } catch (err) {
+            this.setState({ forecast: null, successfulSearch: false });
+          }
+        });
+    }
   }
-
-  forecastSearch = () => {
-    const response = this.mockRequest();
-    console.log(response);
-    this.setState(() => {
-      return {
-        forecast: response.list.map((results) => {
-          return {
-            temp: results.main.temp,
-            temp_min: results.main.temp_min,
-            temp_max: results.main.temp_max,
-            humidity: results.main.humidity,
-            weather_main: results.weather[0].main,
-            weather_description: results.weather[0].description,
-            date: results.dt_txt,
-          };
-        }).filter(forecast => forecast.date.includes('12:00:00')),
-      };
-    });
-  };
-
-
-  // forecastSearch = (city, zipCode) => {
-  //   if (city) {
-  //     return superagent.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city},us&units=imperial&APPID=${APIKEY}`)
-  //       .then((response) => {
-  //         console.log(response);
-  //         this.setState(() => {
-  //           return {
-  //             forecast: response.body.list.map((results) => {
-  //               return {
-  //                 temp: results.main.temp,
-  //                 temp_min: results.main.temp_min,
-  //                 temp_max: results.main.temp_max,
-  //                 humidity: results.main.humidity,
-  //                 weather_main: results.weather[0].main,
-  //                 weather_description: results.weather[0].description,
-  //                 date: results.dt_txt,
-  //               };
-  //             }).filter(forecast => forecast.date.includes('12:00:00')),
-  //             city: response.body.city.name,
-  //           };
-  //         })
-  //         try {
-  //           localStorage.forecast = JSON.stringify(this.state.forecast);
-  //         } catch (err) {
-  //           console.log(err);
-  //           this.setState({ forecast: null, successfulSearch: false });
-  //         }
-  //       });
-  //   }
-  //   else if (zipCode) {
-  //     return superagent.get(`http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode},us&units=imperial&APPID=${APIKEY}`)
-  //       .then((response) => {
-  //         console.log(response);
-  //         this.setState(() => {
-  //           return {
-  //             forecast: response.body.list.map((results) => {
-  //               return {
-  //                 temp: results.main.temp,
-  //                 temp_min: results.main.temp_min,
-  //                 temp_max: results.main.temp_max,
-  //                 humidity: results.main.humidity,
-  //                 weather_main: results.weather[0].main,
-  //                 weather_description: results.weather[0].description,
-  //                 date: results.dt_txt,
-  //               };
-  //             }).filter(forecast => forecast.date.includes('12:00:00')),
-  //             city: response.body.city.name,
-  //           };
-  //         })
-  //         try {
-  //           localStorage.forecast = JSON.stringify(this.state.forecast);
-  //         } catch (err) {
-  //           this.setState({ forecast: null, successfulSearch: false });
-  //         }
-  //       });
-  //   }
-  // }
 
   render() {
     return (
